31eacb6d76faa2e74c5693a4e10b3c8779fa4678
infiniteoo <troydorman@gmail.com>
2023-11-21 13:13:54 -0700

    displaying earth and can spin it

--- src/models/Earth.jsx
+++ src/models/Earth.jsx
@@ -13,15 +13,83 @@ import { a } from "@react-spring/three";
 import earthScene from "../assets/3d/earth.glb";
 import { useFrame, useThree } from "@react-three/fiber";
 
-const Earth = (props) => {
+const Earth = ({
+  isRotating,
+  setIsRotating,
+  setCurrentStage,
+  currentFocusPoint,
+  ...props
+}) => {
   const { gl, viewport } = useThree();
   const earthRef = useRef();
   const { nodes, materials, animations } = useGLTF(earthScene);
   const { actions } = useAnimations(animations, earthRef);
+
+  // Use a ref for the last mouse x position
+  const lastX = useRef(0);
+  // Use a ref for rotation speed
+  const rotationSpeed = useRef(0);
+  // Define a damping factor to control rotation damping
+  const dampingFactor = 0.95;
+
+  useFrame(() => {
+    // If not rotating, apply damping to slow down the rotation (smoothly)
+    if (!isRotating) {
+      // Apply damping factor
+      rotationSpeed.current *= dampingFactor;
+
+      // Stop rotation when speed is very small
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+
+      earthRef.current.rotation.y += rotationSpeed.current;
+    } else {
+      // When rotating, determine the current stage based on island's orientation
+      const rotation = earthRef.current.rotation.y;
+
+      /**
+       * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
+       * The goal is to ensure that the rotation value remains within a specific range to
+       * prevent potential issues with very large or negative rotation values.
+       *  Here's a step-by-step explanation of what this code does:
+       *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
+       *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
+       *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
+       *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
+       *     This is done to ensure that the value remains positive and within the range of
+       *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
+       *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
+       *     modulo operation to the value obtained in step 2. This step guarantees that the value
+       *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
+       *     circle in radians.
+       */
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
   return (
-    <a.group ref={earthRef} {...props} dispose={null}>
+    <group ref={earthRef} {...props} dispose={null}>
       <group name="Sketchfab_Scene">
-        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]} scale={0}>
+        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]} scale={1}>
           <group
             name="5fbd3316863b4d98adffc1dec302e69efbx"
             rotation={[Math.PI / 2, 0, 0]}
@@ -89,7 +157,7 @@ const Earth = (props) => {
           </group>
         </group>
       </group>
-    </a.group>
+    </group>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import { useState, Suspense, useRef, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import Earth from "../models/NewEarth";
+import Space from "../models/Space";
+import SpaceStation from "../models/SpaceStation";
+import { Loader } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
+  const adjustEarthForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, 0, -10]; // Adjust the third value to bring the Earth closer
+
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [50, 50, 50];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [earthScale, earthPosition, earthRotation] = adjustEarthForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className={`w-full h-screen bg-transparent
+  ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
+        camera={{ near: 0.1, far: 1000, position: [0, 0, 1] }} // Adjust the position values
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight itensity={0.5} />
+
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          {/*     <Space isRotating={isRotating} /> */}
+          <Earth
+            position={earthPosition}
+            scale={earthScale}
+            rotation={earthRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+        </Suspense>
+        {/* <Plane
+          position={planePosition}
+          scale={planeScale}
+          isRotating={isRotating}
+          setIsRotating={setIsRotating}
+          rotation={[0, 20, 0]}
+        /> */}
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
